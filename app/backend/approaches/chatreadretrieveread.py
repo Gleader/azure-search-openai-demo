8424be3ce01515dd1309eb8dace2e181f94fed3f
Gleader <mateuszszmidt95@gmail.com>
2023-11-23 16:39:03 +0100

    MSG sys

--- app/backend/approaches/chatreadretrieveread.py
+++ app/backend/approaches/chatreadretrieveread.py
@@ -27,10 +27,10 @@ class ChatReadRetrieveReadApproach(Approach):
     then uses Azure AI Search to retrieve relevant documents, and then sends the conversation history,
     original user question, and search results to OpenAI to generate a response.
     """
-    system_message_chat_conversation = """Assistant helps the company employees with their healthcare plan questions, and questions about the employee handbook. Be brief in your answers.
-Answer ONLY with the facts listed in the list of sources below. If there isn't enough information below, say you don't know. Do not generate answers that don't use the sources below. If asking a clarifying question to the user would help, ask the question.
-For tabular information return it as an html table. Do not return markdown format. If the question is not in English, answer in the language used in the question.
-Each source has a name followed by colon and the actual information, always include the source name for each fact you use in the response. Use square brackets to reference the source, for example [info1.txt]. Don't combine sources, list each source separately, for example [info1.txt][info2.pdf].
+    system_message_chat_conversation = """You are a virtual help desk assistant, responding in the language in which the question was asked, 
+    and your task is to help Żabka's franchisees with technical issues, information about processes such as inventory
+    and emergency situations in store, such as overturned shelves. 
+    If you don't know the answer you don't respond to the question and direct them to the zabka.pl website.
 {follow_up_questions_prompt}
 {injected_prompt}
 """
@@ -42,20 +42,22 @@ Enclose the follow-up questions in double angle brackets. Example:
 Do no repeat questions that have already been asked.
 Make sure the last question ends with ">>"."""
 
-    query_prompt_template = """Below is a history of the conversation so far, and a new question asked by the user that needs to be answered by searching in a knowledge base about employee healthcare plans and the employee handbook.
+    query_prompt_template = """Below is a history of the conversation so far, and a new question asked by the user that needs to be answered by searching in a knowledge base about Żabka's proceses and regulations.
 You have access to an Azure AI Search index with 100's of documents.
 Generate a search query based on the conversation and the new question.
 Do not include cited source filenames and document names e.g info.txt or doc.pdf in the search query terms.
 Do not include any text inside [] or <<>> in the search query terms.
 Do not include any special characters like '+'.
-If the question is not in English, translate the question to English before generating the search query.
+If the question is not in Polish, translate the question to Polish before generating the search query.
 If you cannot generate a search query, return just the number 0.
 """
     query_prompt_few_shots = [
-        {"role": USER, "content": "What are my health plans?"},
-        {"role": ASSISTANT, "content": "Show available health plans"},
-        {"role": USER, "content": "does my plan cover cardio?"},
-        {"role": ASSISTANT, "content": "Health plan cardio coverage"},
+        {"role": USER, "content": "My scanner is broken. What can I do?"},
+        {"role": ASSISTANT, "content": "Skaner, zepsuty"},
+        {"role": USER, "content": "Kiedy powinienem zapłacić za towar."},
+        {"role": ASSISTANT, "content": "Zapłata, towar, termin"},
+        {"role": USER, "content": "Czy mogę sprzedawać, jeśli nie działa mi kasa fiskalna?"},
+        {"role": ASSISTANT, "content": "Awaria kasy fiskalnej, sprzedaż"},
     ]
 
     def __init__(
